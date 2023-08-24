2061bd9083e0948174d7d207ed95d192c88fec0d
abhijith.mr <abhijithgowda1998@gmail.com>
2023-08-24 21:29:58 +0530

    again

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -23,13 +23,14 @@ const BillboardsPage = async ({
         createdAt: format(item.createdAt, "MMMM do, yyyy")
     }));
 
-    
+
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 pt-6">
                 <BillboardClient data={formattedBillboards}/>
             </div>
         </div>
+        
     );
 }
 
