bb2a6ae5d0123ba6c93d90eb3e74076caa1d4210
abhijith.mr <abhijithgowda1998@gmail.com>
2023-08-25 19:27:03 +0530

    New changes

--- app/api/[storeId]/categories/route.ts
+++ app/api/[storeId]/categories/route.ts
@@ -43,10 +43,10 @@ export async function POST(
             }
         })
 
-        return NextResponse.json(billboard);
+        return NextResponse.json(category);
 
     } catch (error) {
-        console.log('[BILLBOARDS_POST]', error);
+        console.log('[CATEGORIES_POST]', error);
         return new NextResponse("Internal error", { status: 500 });
     }
 }
@@ -64,16 +64,16 @@ export async function GET(
             return new NextResponse("Store id is required", { status: 401 });
         }
         
-        const billboards = await prismadb.billboard.findMany({
+        const categories = await prismadb.category.findMany({
             where: {
                 storeId: params.storeId,
             }
         })
 
-        return NextResponse.json(billboards);
+        return NextResponse.json(categories);
 
     } catch (error) {
-        console.log('[BILLBOARDS_GET]', error);
+        console.log('[CATEGORIES_GET]', error);
         return new NextResponse("Internal error", { status: 500 });
     }
 }
